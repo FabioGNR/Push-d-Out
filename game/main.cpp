206510bcb40780cf826a3d8cff1c591f5d5acf82
mattiejas <mattiejas@outlook.com>
2018-09-20 11:30:20 +0200

    Fix main arguments

--- game/main.cpp
+++ game/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <engine/physics/PhysicsManager.h>
 
-int main(int /*argc*/, char* /*argv[]*/) {
+int main(int argc, char* argv[]) {
     std::cout << "Hello, World!\n";
     PhysicsManager pm {};
     pm.doPhysics();
