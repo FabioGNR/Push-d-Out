35818962b2c99c40b78af9170d8461a04759f517
Joris-PC\Joris <jorisjellewillig@gmail.com>
2018-10-08 15:35:16 +0200

    Add CharacterSpawnComponent

---
+++ game/components/CharacterSpawnComponent.h
@@ -0,0 +1,17 @@
+//
+// Created by Joris on 8-10-2018.
+//
+
+#pragma once
+
+#include <ecs/Component.h>
+
+namespace game {
+namespace components {
+    struct CharacterSpawnComponent : public engine::BaseComponent<CharacterSpawnComponent> {
+        double width, height;
+
+        CharacterSpawnComponent() {};
+    };
+}
+}
