c62ea8ff21dc918ba2ca2bf9d242ea988f071ff2
Joris-PC\Joris <jorisjellewillig@gmail.com>
2018-10-08 16:12:10 +0200

    Add LevelMetaComponent

---
+++ game/components/LevelMetaComponent.h
@@ -0,0 +1,25 @@
+//
+// Created by Joris on 8-10-2018.
+//
+
+#pragma once
+
+#include <ecs/Component.h>
+#include <string>
+#include "game/level/LevelDomain.h"
+
+namespace game {
+namespace components {
+    struct LevelMetaComponent : public engine::BaseComponent<LevelMetaComponent> {
+        std::string name;
+        levelDomain::Theme theme;
+        int height, width;
+
+        LevelMetaComponent(std::string name, levelDomain::Theme theme, int height, int width)
+            : name(name),
+              theme(theme),
+              height(height),
+              width(width) {};
+    };
+}
+}
