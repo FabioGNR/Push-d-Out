0abb8dcee969eb8438e05afb8bb06a047211a2c3
Jeffrey Dufour <17874861+oblekta@users.noreply.github.com>
2018-10-09 13:42:30 +0200

    Automatically add component manager (#24)

--- engine/exceptions/ComponentManagerNotFoundException.h
+++
@@ -1,7 +0,0 @@
-#include <exception>
-
-struct ComponentManagerNotFoundException : public std::exception {
-    const char* what() const noexcept override {
-        return "ComponentManager not found of the specified Component type";
-    }
-};
