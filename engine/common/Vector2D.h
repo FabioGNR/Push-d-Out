04669302736c9186f1825d2364e3725f4bf39c59
Joris-PC\Joris <jorisjellewillig@gmail.com>
2018-10-10 12:23:38 +0200

    Add rotation, magnitude, dot-product and findangle to Vector2D

--- engine/common/Vector2D.h
+++ engine/common/Vector2D.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <cmath>
 
 namespace common {
 struct Vector2D {
@@ -22,6 +23,7 @@ struct Vector2D {
         return static_cast<int>(y);
     }
 
+    // basic arithmetic functions
     Vector2D& operator+=(const Vector2D& other)
     {
         x += other.x;
@@ -67,6 +69,13 @@ struct Vector2D {
         return left;
     }
 
+    Vector2D& operator/=(double factor)
+    {
+        x /= factor;
+        y /= factor;
+        return *this;
+    }
+
     Vector2D& operator/=(const Vector2D& other)
     {
         x /= other.x;
@@ -82,13 +91,6 @@ struct Vector2D {
         return left;
     }
 
-    Vector2D& operator/=(double factor)
-    {
-        x /= factor;
-        y /= factor;
-        return *this;
-    }
-
     friend Vector2D operator/(Vector2D left,
         double factor)
     {
@@ -97,6 +99,22 @@ struct Vector2D {
         return left;
     }
 
+    Vector2D& operator-=(const Vector2D& other)
+    {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
+    friend Vector2D operator-(Vector2D left,
+                              const Vector2D& right)
+    {
+        left.x -= right.x;
+        left.y -= right.y;
+        return left;
+    }
+
+    // mathematical properties
     Vector2D max(Vector2D other)
     {
         other.x = std::max(x, other.x);
@@ -111,19 +129,28 @@ struct Vector2D {
         return other;
     }
 
-    Vector2D& operator-=(const Vector2D& other)
-    {
-        x -= other.x;
-        y -= other.y;
-        return *this;
+    double magnitude() const {
+        return sqrt(pow(this->x, 2)+pow(this->y, 2));
     }
 
-    friend Vector2D operator-(Vector2D left,
-        const Vector2D& right)
-    {
-        left.x -= right.x;
-        left.y -= right.y;
-        return left;
+    // Vector mathematics
+    Vector2D rotate(double angle) {
+        double newX = this->x*cos(angle) - this->y*sin(angle);
+        double newY = this->x*sin(angle) - this->y*cos(angle);
+
+        return Vector2D(newX, newY);
+    }
+
+    double dotProduct(const Vector2D& other) {
+        return
+            this->x * other.x +
+            this->y * other.y;
+    }
+
+    double findAngle(const Vector2D& other) {
+        double dot = this->dotProduct(other);
+        double cosAngle = dot/(this->magnitude() * other.magnitude());
+        return acos(cosAngle);
     }
 };
 }
