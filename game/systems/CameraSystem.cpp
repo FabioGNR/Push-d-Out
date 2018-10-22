4c8c470c346ca762f1555e855768fb7b2f7e7943
Fabio Waljaard <fabio.waljaard@gmail.com>
2018-10-22 13:14:17 +0200

    Implement less chaotic zooming in camerasystem

--- game/systems/CameraSystem.cpp
+++ game/systems/CameraSystem.cpp
@@ -1,15 +1,23 @@
 #include "CameraSystem.h"
+#include <cmath>
 namespace game {
 namespace systems {
 
     void CameraSystem::update(std::chrono::nanoseconds timeStep)
     {
+        static long timePassed = 0;
+        static bool reverse = false;
         auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(timeStep);
-        auto pos = m_camera->getPosition();
-        pos.x -= milliseconds.count() * 0.002;
-        auto zoom = m_camera->getZoom();
-        zoom -= milliseconds.count() * 0.0001;
-        m_camera->setPosition(pos);
+        timePassed += milliseconds.count();
+        double progress = timePassed/4000.0;
+
+        double baseZoom = 0.75;
+        double zoom = baseZoom + 0.5*(reverse ? 1-progress : progress);
+        if(progress > 1)
+        {
+            timePassed = 0;
+            reverse = !reverse;
+        }
         m_camera->setZoom(zoom);
     }
 
