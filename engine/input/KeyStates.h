39303827ff84626851962bf3bd1f119d0f0a7ba9
Fabio Waljaard <FabioGNR@users.noreply.github.com>
2018-10-23 14:19:03 +0200

    Input manager unit tests (#39)
    * Write tests for KeyMap and fix bugs found by it
    
    * Consistent test naming

--- engine/input/KeyStates.h
+++ engine/input/KeyStates.h
@@ -3,14 +3,9 @@
 namespace engine {
 namespace input {
     /*
-     *              8 4 2 1
-     * DOWN         0 0 1 0
-     * PRESSED      0 0 1 1
-     * RELEASED     0 1 0 0
-     * PRESSED/REL. 0 1 1 1
-     * UP           1 1 0 0
      *
      * Returns true when result equals request, e.g.
+     * See unit test KeyMap_test.cpp for all expectations
      *
      * ACTUAL STATE | REQUEST       | RETURNS       | STATE & REQUEST
      * -------------|---------------|---------------|---------------
@@ -22,15 +17,15 @@ namespace input {
      * PRESSED      | DOWN          | true          | 0010
      * RELEASED     | DOWN          | false         | 0000
      * PRESSED/REL. | DOWN          | true          | 0010
-     * UP           | RELEASED      | true          | 0100
+     * RELEASED     | UP            | true          | 0100
      * etc...
      */
     enum KeyStates {
-        DOWN = 2,
-        PRESSED = 3,
-        RELEASED = 4,
-        PRESSED_AND_RELEASED = 7,
-        UP = 12
+        DOWN = 0b0010,
+        PRESSED = 0b0011,
+        UP = 0b0100,
+        RELEASED = 0b1100,
+        PRESSED_AND_RELEASED = 0b1111
     };
 }
 }
