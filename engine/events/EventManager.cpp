3991a1b7b731d3d78f0e778d2bd751d232bc49cd
Fabio Waljaard <fabio.waljaard@gmail.com>
2018-11-20 18:09:45 +0100

    Fix small clang-tidy warning

--- engine/events/EventManager.cpp
+++ engine/events/EventManager.cpp
@@ -11,9 +11,9 @@ namespace events {
 
         m_inputManager.update();
         for (const auto& event : events) {
-            if (dynamic_cast<IControlEvent*>(event.get())) {
+            if (dynamic_cast<IControlEvent*>(event.get()) != nullptr) {
                 m_inputManager.handle(std::move(event));
-            } else if (dynamic_cast<QuitEvent*>(event.get())) { // TODO: eventually remove this
+            } else if (dynamic_cast<QuitEvent*>(event.get()) != nullptr) { // TODO: eventually remove this
                 return false;
             }
         }
