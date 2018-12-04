abac105a8aa615bfc2e3ee6ed9f6567e8655421f
RvdM <rvdm1152@gmail.com>
2018-12-04 12:56:32 +0100

    clang

--- engine/input/SDLKeys.h
+++ engine/input/SDLKeys.h
@@ -9,8 +9,8 @@ using SDL_Keycode = int;
 namespace engine {
 namespace input {
     struct SDLKeys {
-        static const int mouseStart = 813;
         static const int conStart = 850;
+        static const int mouseStart = 813;
         static const std::map<SDL_Keycode, Keys> KEYCODES;
         static input::Keys get(int SDL_Key);
     };
