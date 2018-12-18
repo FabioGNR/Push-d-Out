bbfd7f0964863b71a223da9a3606dd635ef2bd13
Matthias Aarnoutse <mattiejas@outlook.com>
2018-12-18 14:44:09 +0100

    📷  (#98)
    * More dynamic camera still broken though
    
    * Tweak some camera values, still sucks
    
    * Center camera on target
    
    * Zoom while targeting players
    
    * Zoom smoother
    
    * Clean-up
    
    * clang-format
    
    * Process review
    
    * Move camera faster if player gets out of safe zone
    
    * Center camera in level on init
    
    * clang-format

--- game/systems/CameraSystem.h
+++ game/systems/CameraSystem.h
@@ -7,29 +7,43 @@
 #include <engine/graphics/IRenderer.h>
 #include <memory>
 
-namespace game {
-namespace systems {
-    class CameraSystem : public engine::ecs::BaseSystem<CameraSystem> {
-    public:
-        explicit CameraSystem(engine::ecs::World* ecsWorld, engine::graphics::Camera* camera)
-            : m_camera{ camera }
-            , m_ecsWorld{ ecsWorld }
-        {
-        }
-
-        void update(std::chrono::nanoseconds timeStep) override;
-        void render(engine::IRenderer& renderer) override;
-
-    private:
-        engine::graphics::Camera* m_camera;
-        engine::ecs::World* m_ecsWorld;
-        bool findPlayerCenterOfMass(common::Vector2D<double>& center,
-            common::Vector2D<double>& lower,
-            common::Vector2D<double>& upper);
-        common::Vector2D<double> getLevelDimensions() const;
-        void centerOnLevel();
-        bool isEntityWithinRegion(const engine::ecs::Entity& entity, const common::Vector2D<double>& region) const;
-        common::Vector2D<double> clampCameraPosition(common::Vector2D<double> pos) const;
+namespace game::systems {
+class CameraSystem : public engine::ecs::BaseSystem<CameraSystem> {
+public:
+    explicit CameraSystem(engine::ecs::World* ecsWorld, engine::graphics::Camera* camera)
+        : m_camera{ camera }
+        , m_ecsWorld{ ecsWorld }
+    {
+        m_currentPosition = m_camera->getPosition();
     };
-}
+
+    void update(std::chrono::nanoseconds timeStep) override;
+    void render(engine::IRenderer& renderer) override;
+
+private:
+    static const common::Vector2D<double> PADDING;
+    static const common::Vector2D<double> CAMERA_SAFE_ZONE_MARGIN;
+    static constexpr double CAMERA_MOVEMENT_SPEED = 3;
+    static constexpr double CAMERA_MOVEMENT_SPEED_FAST = 20;
+
+    common::Vector2D<double> m_currentPosition;
+    common::Vector2D<double> m_lastCenter;
+    double m_moveSpeed;
+
+    engine::graphics::Camera* m_camera;
+    engine::ecs::World* m_ecsWorld;
+
+    struct Boundaries {
+        common::Vector2D<double> center;
+        common::Vector2D<double> size;
+    };
+
+    void setCameraMoveSpeed();
+    Boundaries findTargetBoundaries();
+    common::Vector2D<double> getLevelDimensions() const;
+    double findSmoothZoom(Boundaries boundaries, double seconds);
+    common::Vector2D<double> findNextPosition(Boundaries boundaries, double seconds);
+    bool isWithinLevel(common::Vector2D<double> point, common::Vector2D<double> dimensions) const;
+    bool isOutOfSafeCameraBounds(common::Vector2D<double> point, common::Vector2D<double> dimensions) const;
+};
 }
